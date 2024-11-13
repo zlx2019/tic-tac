1765bf9601eaad33c1966ea41a946968f8e7aad3
Zero <zero9501@outlook.com>
2024-11-13 23:18:24 +0800

    feat: code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,72 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function App() { 
-    // 游戏参数.
-    const args = {
-        layoutRow: 3,
-        layoutCol: 3
-    }
-    return (
-        <div>
-            <Layout {...args} />
-        </div>
-    )
+export default function App() {
+  const args = {
+    rows: 3,    // 棋盘行数
+    columns: 3, // 棋盘列数
+  };
+  return (
+    <div>
+      <Layout {...args} />
+    </div>
+  );
 }
-
 
 // 棋盘布局
-// col: 列数
-// row: 行数
-function Layout({ layoutCol, layoutRow }) {
-    // 创建 col * row 二维数组.
-    const board = Array(layoutRow).fill(null).map(() => Array(layoutCol).fill(null))
-    const [squares, setSquares] = useState(Array(layoutRow).fill(null).map(() => Array(layoutCol).fill(null)))
-    // const handleClick = (row, col) => {
-    //     const temp = squares.slice()
-    //     temp[row][col] = 'X'
-    //     setSquares(temp)
-    // }
+function Layout({ rows, columns }) {
+  // 创建二维数组棋盘 共 (rows * columns) 个方格
+  // X    X    X
+  // X    X    X
+  // X    X    X
+  const board = Array(rows).fill(null).map(() => Array(columns).fill(null));
 
-    // 点击方块事件函数
-    const handleClick = (row, col) => {
-        setSquares(prevSquares => {
-            const newSquares = prevSquares.map((r, rowIndex) => 
-                rowIndex === row ? r.map((cell, colIndex) => colIndex === col ? 'X' : cell) : r
-            )
-            return newSquares
-        })
-    }
+  // 创建 (rows * columns) 个状态，表示每个方格的状态.
+  // 状态有三种: null/X/O, 默认为 null
+  const [squaresStatus, setSquaresStatus] = useState(
+    Array(rows)
+      .fill(null)
+      .map(() => Array(columns).fill(null))
+  );
 
-    return (
-        <>
-            {board.map((row, rowIdx) => (
-                <div key={rowIdx} className="board-row">
-                    {row.map((_, colIdx) => (
-                        <Square key={`${rowIdx}-${colIdx}`} value={squares[rowIdx][colIdx]} onSquareClick={ ()=> handleClick(rowIdx, colIdx) }/>
-                    )) }
-                </div>
-            )) }
-        </>
-    )
+  // 状态监听器, 当 squares 发生变化时回调
+  useEffect(() => {
+    console.log(`战况发生变化: `, squaresStatus);
+  }, [squaresStatus]);
+
+  /**
+   * 棋盘点击事件
+   * @param r & c: 被点击的方块位置
+   */
+  const handleClick = (r, c) => {
+    // 遍历棋盘状态二维数组，找到点击的方块并修改值
+    setSquaresStatus((prevStatus) => {
+      const newStatus = prevStatus.map((row, rowIdx) => {
+        return rowIdx === r
+          ? row.map((column, colIndex) => (colIndex === c ? "X" : column))
+          : row;
+      });
+      return newStatus;
+    });
+  };
+
+  return (
+    <>
+      {board.map((row, rowIdx) => (
+        <div key={rowIdx} className="board-row">
+          {row.map((_, colIdx) => (
+            <Square
+              key={`${rowIdx}-${colIdx}`}
+              value={squaresStatus[rowIdx][colIdx]}
+              onSquareClick={() => handleClick(rowIdx, colIdx)}
+            />
+          ))}
+        </div>
+      ))}
+    </>
+  );
 }
 
-// 方块
+// 棋局单个方块
 function Square({ value, onSquareClick }) {
-    return <button className="square" onClick={ onSquareClick } > { value } </button>
-
+  return <button className="square" onClick={onSquareClick}> {value} </button>
 }
