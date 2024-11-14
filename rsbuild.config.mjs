f4d07aceda32f3d8973f3865eb7e051f109f09e3
Zero <zero9501@outlook.com>
2024-11-14 09:43:37 +0800

    feat: add config

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,9 +1,19 @@
+// RsBuild 全局配置
+
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   html: {
-    title: 'Tic-Tac'
+    template: 'index.html'
   },
+  source: {
+    // 设置别名
+    alias: {
+      '@': './src',
+      "@components": "@/components"
+    }
+  },
+  // 注册插件
   plugins: [pluginReact()],
 });
